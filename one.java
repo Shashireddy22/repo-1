42f085b5153499d3a854fea6e505d91be3fcbef6
shashireddyv <108355204+shashireddyv@users.noreply.github.com>
2022-07-16 12:27:12 +0530

    one.java
    one.java file Added

---
+++ one.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+class one
+{
+   public static void main(string[] args)
+   {
+     system.ouy.println("Welcome To Java Application-1");
+      system.ouy.println("Welcome To Java Application-1");
+      system.ouy.println("Welcome To Java Application-1");
+      system.ouy.println("Welcome To Java Application-1");
+   }
+}
